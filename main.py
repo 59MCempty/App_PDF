519d766a11b9c046d1f5a5708aa09ea156ffaacc
MCEmptyY <cuong125a@gmail.com>
2023-03-15 15:16:26 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from fpdf import FPDF
+import pandas
+
 
 # P for Portrait, L B for Landscape
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pandas.read_csv("topics.csv")
 
-# set cell:
 """
 h: height of cell
 ln: break line 
 if ln=0 --> next cell will be added after tail of this cell
 border: create square
-"""
+
+# set cell:
 pdf.set_font(family="Times", style="B", size=12)
 pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
 pdf.set_font(family="Times", style="I", size=10)
 pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1, border=0)
+"""
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)  # gray color
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
+    # pdf.line(x1, x2, y1, y2)
+    # x1, y1: coordinates of starting point of line
+    # value of x1: from left border to starting point
+    # x2, y2: coordinates of the end point of line
+    # value of x2: distance from starting point to the end point
+    pdf.line(20, 21, 200, 21)
 
 pdf.output("output.pdf")
